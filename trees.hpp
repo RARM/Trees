be3b741636f4baa6e28ba1f2a6a108ab1986c513
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2021-10-16 11:15:12 -0400

    Added manual insertions and deletions for the general tree.
    These have not been tested completely. The traversal operation are 
    needed for a complete test.

--- trees.hpp
+++ trees.hpp
@@ -12,7 +12,7 @@ namespace rarm_trees
 	class Binary_Tree
 	{
 	public:
-		Binary_Tree(T data_val = NULL);
+		Binary_Tree(T data_val, Binary_Tree* left = nullptr, Binary_Tree* Right = nullptr);
 		Binary_Tree(const Binary_Tree& source); // Deep copy.
 		~Binary_Tree();
 
@@ -60,29 +60,64 @@ namespace rarm_trees
  * 
  * Read more about this issue here:
  * https://isocpp.org/wiki/faq/templates#separate-template-fn-defn-from-decl
+ * 
+ * After finishinf this header, I will probably separate them
+ * with the solution presented the in the link above.
 */
 
+// ---------------------- BINARY TREE ----------------------
 // General constructor for the Binary_Tree class.
 template <typename T>
-rarm_trees::Binary_Tree<T>::Binary_Tree(T data_val)
+rarm_trees::Binary_Tree<T>::Binary_Tree(T data_val, Binary_Tree* left, Binary_Tree* right)
 	: data { data_val }
 {
-	this->left = nullptr;
-	this->right = nullptr;
+	// Left tree.
+	this->left = (left == nullptr) ? nullptr : new Binary_Tree(*left);
+
+	// Right tree.
+	this->right = (right == nullptr) ? nullptr : new Binary_Tree(*right);
 }
 
 // Copy constructor.
 template <typename T>
 rarm_trees::Binary_Tree<T>::Binary_Tree(const Binary_Tree& source)
-	: Binary_Tree(source.data, *source.left, *source.right)
-{}
+	: data { source.data }
+{
+	// Left tree.
+	this->left = (source.left == nullptr) ? nullptr : new Binary_Tree(*(source.left));
+
+	// Right tree.
+	this->right = (source.right == nullptr) ? nullptr : new Binary_Tree(*(source.right));
+}
 
 // Desctructor.
 template <typename T>
 rarm_trees::Binary_Tree<T>::~Binary_Tree()
 {
+	if (this->left != nullptr) delete this->left;
+	if (this->right != nullptr) delete this->right;
 	return;
 }
+
+// Manual insertions.
+template <typename T>
+void rarm_trees::Binary_Tree<T>::manual_ins_left(Binary_Tree* data)
+{ this->left = data; return; }
+
+template <typename T>
+void rarm_trees::Binary_Tree<T>::manual_ins_right(Binary_Tree* data)
+{ this->right = data; return; }
+
+// Manual deletions.
+// These functions will recursively delete a node and all its childrens.
+// The recursion is done in the destructor.
+template <typename T>
+void rarm_trees::Binary_Tree<T>::manual_del_left()
+{ delete this->left; return; }
+
+template <typename T>
+void rarm_trees::Binary_Tree<T>::manual_del_right()
+{ delete this->right; return; }
 
 // Get value.
 template <typename T>
